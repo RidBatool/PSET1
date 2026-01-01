176be5c234191f25c9899c4474657c0900b5acbe
Rida Batool <ridabatoolghori@gmail.com>
2026-01-01 12:29:42 +0500

    Task1

---
+++ Logic_task1/main.py
@@ -0,0 +1,15 @@
+def contribution(list_):
+    impact=[1]*len(list_)
+
+    pre_product=1
+    for i in range(len(list_)):
+        impact[i]*=pre_product
+        pre_product*=list_[i]
+    pos_product=1
+    for j in range(len(list_)-1, -1, -1):
+        impact[j]*=pos_product
+        pos_product*=list_[j]
+    return impact
+
+print(contribution([-1,1,0,-3,3]))  # Output: [24, 12, 8, 6]
+
